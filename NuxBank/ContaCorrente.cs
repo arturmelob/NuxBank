0b22c089f4164aaa54d1f86887c18e9da937b173
Artur Melo Barbosa <arturbarbosati@gmail.com>
2018-12-14 17:02:00 -0200

    Commit ContaCorrente, Cliente, e algumas exceções

---
+++ NuxBank/ContaCorrente.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuxBank
+{
+    public class ContaCorrente
+    {
+        public int NumeroAgencia { get; set; }
+        public int NumeroConta { get; set; }
+        public Cliente Titular { get; set; }
+        private decimal _saldo { get; set; }
+        public decimal Saldo {
+
+            get {
+                return _saldo;
+            }
+
+            set {
+
+                if (value < 0)
+                    return;
+                _saldo = value;
+            }
+        }
+        public int TotalDeContasCriadas { get; set; }
+        public bool Status { get; set; }
+
+        public ContaCorrente(int agencia, int conta){
+
+            if(agencia <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            TotalDeContasCriadas++;
+        }
+
+        public void Sacar(ContaCorrente conta, decimal valorDeSaque)
+        {
+            if(conta.Saldo <= valorDeSaque)
+            {
+                throw new SaldoInsuficienteException();
+            }
+
+        }
+
+    }
+}
---
+++ NuxBank/SaldoInsuficienteException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NuxBank
+{
+    public class SaldoInsuficienteException : Exception
+    {
+        public decimal Saldo { get; set; }
+        public decimal ValorSaque { get; set; }
+        public SaldoInsuficienteException()
+        {
+        }
+
+        public SaldoInsuficienteException(decimal saldo, decimal valorSaque)
+                : this($"Tentativa de realizar saque no valor de {valorSaque} em uma conta de saldo {saldo}")
+        {
+        }
+
+        public SaldoInsuficienteException(string message)
+            : base(message)
+        {
+        }
+
+        public SaldoInsuficienteException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+           }
+
+    }
+}
